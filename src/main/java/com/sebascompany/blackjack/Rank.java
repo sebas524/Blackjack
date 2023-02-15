c6abaf3c9abf4c0a76f4068f9e8768d97e973c55
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-02-15 17:41:24 +0100

    Initial commit

---
+++ src/main/java/com/sebascompany/blackjack/Rank.java
@@ -0,0 +1,26 @@
+package com.sebascompany.blackjack;
+
+public enum Rank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+    public int getValue() {
+        return switch (this) {
+            case JACK, QUEEN, KING -> 10;
+            default -> ordinal() + 1;
+        };
+    }
+
+
+}
